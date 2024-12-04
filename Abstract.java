c5e52e611b29f23380affbe417789c5012f01b52
Thiru344 <t9063357@gmail.com>
2024-12-04 14:53:07 +0530

    Add files via upload

---
+++ Abstract.java
@@ -0,0 +1,27 @@
+ interface India {
+ int a = 10;
+ public int external(int b);
+ public char affairs(char ch);
+ public String jaishankar(string s);
+ public float deplamatic(float f){
+ System.out.println("ajith is a famous deplamative: "+f);
+ }
+ }
+ public abstract class Russia implements India{
+ public static void main(String[] args){
+ Russia r= new Russia();
+ public int external(int b){
+ System.out.println("ajith is a external officer: "+b);
+ }
+ public char affairs(char ch){
+ System.out.println("name of the character is : "+ch);
+ }
+  abstract String jaishankar(String s);
+  abstract float deplamatic(float f);
+ r.external(7);
+ r.affairs('j');
+ 
+}
+} 
+ 
+ 
